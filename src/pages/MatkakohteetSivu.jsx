ee150fe396e78fcffacb67a758fc800716666e5b
Pahkiz <pasanen.juho@gmail.com>
2022-02-23 10:05:47 +0200

    Kohdekortin ja Matkakohdesivun aloittelua

--- src/pages/MatkakohteetSivu.jsx
+++ src/pages/MatkakohteetSivu.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Kohdekortti from "../MatkakohteetSivu/Kohdekortti";
 
 const MatkakohteetSivu = () => {
+  function KohdeLista() {
+    return (
+      <Kohdekortti /> // Kohdekortti on ReusableComponents kansiossa
+    );
+  }
+
   return (
     <Wrapper>
       <h1>MatkakohteetSivu</h1>
+
+      <body>
+        <div>
+          <input type="text" className="searchBar" placeholder="Hae kohteita">
+            <button className="SearchButton" onClick={() => handleFetch()}>
+              Hae
+            </button>
+          </input>
+        </div>
+        <table></table>
+      </body>
     </Wrapper>
   );
 };
