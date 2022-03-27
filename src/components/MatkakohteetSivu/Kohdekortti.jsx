ef02b4c56029f807401cae5ecc8259146914ee64
Remmeli <juha.myller@protonmail.com>
2022-03-27 20:39:49 +0300

    CSS säätöä

--- src/components/MatkakohteetSivu/Kohdekortti.jsx
+++ src/components/MatkakohteetSivu/Kohdekortti.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import ImageContainer from "../ResuableComponents/ImageContainer";
-import {
-  Link,
-  useLocation,
-  useNavigate,
-  NavLink,
-  Routes,
-  Route,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import "./Kohdekortti.css";
 
 const Kohdekortti = ({ img, maa, tarinat, kohde }) => {
