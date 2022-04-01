648572dbd107db44862418ad41abca2b658d9317
JuhaMyller <juha.myller@protonmail.com>
2022-04-01 03:23:13 +0300

    Route tarina/:id sivulle

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import {
   LisaaTarinaSivu,
   Kirjaudu,
   Rekisteröidy,
-} from './pages';
-import Navbar from './components/NavBar/Navbar';
-import { useSelector, useDispatch } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
+} from "./pages";
+import Navbar from "./components/NavBar/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import {
   KirjauduRefreshToken,
   KirjauduSpostiJaSalasana,
-} from './Redux/Actions/authActions';
+} from "./Redux/Actions/authActions";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import TarinaSivu from "./pages/TarinaSivu";
 
 function App() {
   const user = useSelector((state) => state.auth.kayttaja);
@@ -41,6 +42,7 @@ function App() {
           <Route exact path="matkakohteet" element={<MatkakohteetSivu />} />
           <Route exact path="matkakohteet/:id" element={<MatkakohdeIDSivu />} />
           <Route exact path="lisaatarina" element={<LisaaTarinaSivu />} />
+          <Route exact path="tarina/:id" element={<TarinaSivu />} />
           <Route exact path="kirjaudu" element={<Kirjaudu />} />
           <Route exact path="rekisteroidy" element={<Rekisteröidy />} />
         </Routes>
