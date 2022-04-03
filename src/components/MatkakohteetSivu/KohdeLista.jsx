282b24464b4ffd13e25259cb7da73dd08963b7ff
Remmeli <juha.myller@protonmail.com>
2022-04-03 15:37:05 +0300

    linkit toimii matkakohteen ID:n mukaan, piilotettu matkakohteen muokkausnappi jos et ole kirjautunut

--- src/components/MatkakohteetSivu/KohdeLista.jsx
+++ src/components/MatkakohteetSivu/KohdeLista.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import './Kohdekortti.css';
-import Kohdekortti from './Kohdekortti';
-import Input from '../ResuableComponents/Input';
-import Button from '../ResuableComponents/Button';
-import axios from '../../api/Axios';
-import { useModalContext } from '../ResuableComponents/Modal/ModalContext';
-import MatkakohteetTemplate from '../ModalTemplates/MatkakohteetTemplate/MatkakohteetTemplate';
+import React, { useEffect, useState } from "react";
+import "./Kohdekortti.css";
+import Kohdekortti from "./Kohdekortti";
+import Input from "../ResuableComponents/Input";
+import Button from "../ResuableComponents/Button";
+import axios from "../../api/Axios";
+import { useModalContext } from "../ResuableComponents/Modal/ModalContext";
+import MatkakohteetTemplate from "../ModalTemplates/MatkakohteetTemplate/MatkakohteetTemplate";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const BASE_URL = "https://ohjelmistotuotanto2.herokuapp.com";
 
 const KohdeLista = () => {
   const [mkohteet, setMkohteet] = useState([]);
-  const [etsi, setEtsi] = useState('');
+  const [etsi, setEtsi] = useState("");
   const [filteredData, setFilteredData] = useState([]);
-
   const { openModal } = useModalContext();
+  const user = useSelector((state) => state.auth.kayttaja);
 
   const getMatkakohteet = async () => {
-    const response = await axios.get('/api/matkakohde/matkakohteet');
+    const response = await axios.get("/api/matkakohde/matkakohteet");
     setMkohteet(response.data.matkakohteet);
     setFilteredData(response.data.matkakohteet);
     console.log(response.data);
   };
 
   useEffect(() => {
-    console.log('use');
     getMatkakohteet();
   }, []);
 
   const filter = () => {
-    if (etsi !== '') {
+    if (etsi !== "") {
       const results = mkohteet.filter((matkakohde) => {
         return matkakohde.kohdenimi
           .toLowerCase()
@@ -54,19 +56,19 @@ const KohdeLista = () => {
               onChange={setEtsi}
               placeholder="Etsi"
               styles={{
-                marginTop: '10px',
-                marginBottom: '10px',
-                float: 'left',
-                width: '50%',
-                marginLeft: '100px',
+                marginTop: "10px",
+                marginBottom: "10px",
+                float: "left",
+                width: "50%",
+                marginLeft: "100px",
               }}
             />
             <Button
               styles={{
-                marginLeft: '2px',
-                float: 'left',
-                marginTop: '10px',
-                marginBottom: '10px',
+                marginLeft: "2px",
+                float: "left",
+                marginTop: "10px",
+                marginBottom: "10px",
               }}
               onClick={filter}
               className="button"
@@ -74,34 +76,42 @@ const KohdeLista = () => {
               Etsi
             </Button>
           </div>
-          <Button
-            styles={{
-              marginRight: '100px',
-              marginTop: '10px',
-              marginBottom: '10px',
-            }}
-            onClick={() =>
-              openModal({
-                template: <MatkakohteetTemplate mkohteet={mkohteet} />,
-                title: 'Muokkaa matkakohteita',
-              })
-            }
-          >
-            Muokkaa
-          </Button>
+          {!user ? null : (
+            <div>
+              <Button
+                styles={{
+                  marginRight: "100px",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                }}
+                onClick={() =>
+                  openModal({
+                    template: <MatkakohteetTemplate mkohteet={mkohteet} />,
+                    title: "Muokkaa matkakohteita",
+                  })
+                }
+              >
+                Muokkaa
+              </Button>
+            </div>
+          )}
         </div>
       </div>
       <div>
         <div className="kohdekortti_lista">
           {filteredData && filteredData.length > 0 ? (
             filteredData.map((matkakohde) => (
-              <Kohdekortti
-                kuva={`${process.env.REACT_APP_SERVER_URL}/img/${matkakohde.kuva}`}
-                kohdenimi={matkakohde.kohdenimi.toUpperCase()}
-                maa={matkakohde.maa}
-                id={matkakohde._id}
+              <Link
+                to={`/matkakohteet/:${matkakohde._id}`}
                 key={matkakohde._id}
-              ></Kohdekortti>
+              >
+                <Kohdekortti
+                  kuva={`${BASE_URL}/img/${matkakohde.kuva}`}
+                  kohdenimi={matkakohde.kohdenimi.toUpperCase()}
+                  maa={matkakohde.maa}
+                  id={matkakohde._id}
+                ></Kohdekortti>
+              </Link>
             ))
           ) : (
             <h2>Haulla ei löytynyt mitään</h2>
--- src/components/MatkakohteetSivu/Kohdekortti.jsx
+++ src/components/MatkakohteetSivu/Kohdekortti.jsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import ImageContainer from '../ResuableComponents/ImageContainer';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
-import './Kohdekortti.css';
+import React from "react";
+
+import "./Kohdekortti.css";
 
 const Kohdekortti = ({ kuva, maa, kohdenimi, id }) => {
   return (
-    <Link to={`/matkakohteet/${id}`}>
-      <div className="kohdekortti" style={{ backgroundImage: `url(${kuva})` }}>
-        <div className="kohdekortti_kohde_maa">
-          <h1>{kohdenimi}</h1>
-          <h4>{maa}</h4>
-        </div>
-        <div className="kohdekortti_tarinat">
-          <p>Tarinoita</p>
-        </div>
+    <div className="kohdekortti" style={{ backgroundImage: `url(${kuva})` }}>
+      <div className="kohdekortti_kohde_maa">
+        <h1>{kohdenimi}</h1>
+        <h4>{maa}</h4>
       </div>
-    </Link>
+      <div className="kohdekortti_tarinat">
+        <p>Tarinoita</p>
+      </div>
+    </div>
   );
 };
 
