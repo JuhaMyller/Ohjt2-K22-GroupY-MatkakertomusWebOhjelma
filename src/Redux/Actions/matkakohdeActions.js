0c6dbdb454071285eff5e3ebb960d6231460d306
MarekPuu <marek.puurunen@gmail.com>
2022-04-07 17:32:31 +0300

    Hakeminen, poistaminen ja lisääminen tehty

--- src/Redux/Actions/matkakohdeActions.js
+++ src/Redux/Actions/matkakohdeActions.js
@@ -1,5 +1,10 @@
 export const ADD_MATKAKOHDE = 'ADD_MATKAKOHDE';
-export const DELETE_MATKAKOHDE = 'DELETE_MATKAKOHDE';
+export const DELETE_MATKAKOHDE_ERROR = 'DELETE_MATKAKOHDE_ERROR';
+export const DELETE_MATKAKOHDE_SUCCESS = 'DELETE_MATKAKOHDE_SUCCESS';
+export const REQUEST_BEGIN = 'REQUEST_BEGIN';
+export const POST_MATKAKOHDE_SUCCESS = 'POST_MATKAKOHDE_SUCCESS';
+export const POST_MATKAKOHDE_ERROR = 'POST_MATKAKOHDE_ERROR';
+export const GET_MATKAKOHTEET_SUCCESS = 'GET_MATKAKOHTEET_SUCCESS';
 
 export function addMatkakohde(payload) {
   return {
@@ -7,9 +12,78 @@ export function addMatkakohde(payload) {
     payload,
   };
 }
-export function deleteMatkakohde(payload) {
-  return {
-    type: DELETE_MATKAKOHDE,
-    payload: payload,
+export function deleteMatkakohde(id, axios, toast) {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST_BEGIN });
+      const response = await axios.delete('/api/matkakohde/matkakohde/' + id);
+      dispatch({
+        type: DELETE_MATKAKOHDE_SUCCESS,
+        payload: response.data.matkakohde,
+      });
+      toast.success('Matkakohde poistettu', {
+        position: 'top-center',
+        duration: 1500,
+      });
+    } catch (error) {
+      dispatch({ type: DELETE_MATKAKOHDE_ERROR });
+      toast.error(error.response.data.message, {
+        position: 'top-center',
+        duration: 1500,
+      });
+    }
+  };
+}
+
+export function getMatkakohteet(axios) {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get('/api/matkakohde/matkakohteet');
+      if (response)
+        dispatch({
+          type: GET_MATKAKOHTEET_SUCCESS,
+          payload: response.data.matkakohteet,
+        });
+    } catch (error) {}
+  };
+}
+
+export function postMatkakohde(payload, toast, axios) {
+  const { matkakohde, maa, paikkakunta, matkanKuvaus, kuvat } = payload;
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST_BEGIN });
+      try {
+        const formData = new FormData();
+        formData.append('kohdenimi', matkakohde);
+        formData.append('maa', maa);
+        formData.append('paikkakunta', paikkakunta);
+        formData.append('kuvateksti', matkanKuvaus);
+        formData.append('kuva', kuvat[0]);
+
+        const response = await axios.post(
+          'api/matkakohde/matkakohde',
+          formData
+        );
+        if (response.status === 201) {
+          dispatch({
+            type: POST_MATKAKOHDE_SUCCESS,
+            payload: response.data.uusiMatkakohde,
+          });
+          toast.success('Matkakohde luotu', {
+            position: 'top-center',
+            duration: 1500,
+          });
+        }
+      } catch (error) {
+        dispatch({
+          type: POST_MATKAKOHDE_ERROR,
+        });
+        toast.error(error.response.data.message, {
+          position: 'top-center',
+          duration: 1500,
+        });
+      }
+    } catch (error) {}
   };
 }
--- src/Redux/Reducers/matkakohdeReducer.js
+++ src/Redux/Reducers/matkakohdeReducer.js
@@ -1,26 +1,50 @@
 import {
   ADD_MATKAKOHDE,
-  DELETE_MATKAKOHDE,
+  DELETE_MATKAKOHDE_SUCCESS,
+  DELETE_MATKAKOHDE_ERROR,
+  REQUEST_BEGIN,
+  POST_MATKAKOHDE_SUCCESS,
+  POST_MATKAKOHDE_ERROR,
+  GET_MATKAKOHTEET_SUCCESS,
 } from '../Actions/matkakohdeActions';
 const initialState = {
   Matkakohteet: [],
+  fetchingRequest: false,
 };
 
 export default function matkakohdeReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_BEGIN:
+      return { ...state, fetchingRequest: true };
     case ADD_MATKAKOHDE:
       return {
         ...state.Matkakohteet,
         matkakohteet: state.Matkakohteet.concat(action.payload),
       };
-    case DELETE_MATKAKOHDE:
-      const matkakohdeID = action.payload.id;
+
+    case POST_MATKAKOHDE_SUCCESS:
       return {
-        ...state.Matkakohteet,
-        matkakohteet: state.Matkakohteet.filter(
-          (matkakohde) => matkakohde.id !== matkakohdeID
+        ...state,
+        fetchingRequest: false,
+        Matkakohteet: [...state.Matkakohteet, action.payload],
+      };
+
+    case POST_MATKAKOHDE_ERROR:
+      return { ...state, fetchingRequest: false };
+
+    case GET_MATKAKOHTEET_SUCCESS:
+      return { ...state, Matkakohteet: action.payload };
+
+    case DELETE_MATKAKOHDE_SUCCESS:
+      return {
+        ...state,
+        fetchingRequest: false,
+        Matkakohteet: state.Matkakohteet.filter(
+          (matkakohde) => matkakohde._id !== action.payload
         ),
       };
+    case DELETE_MATKAKOHDE_ERROR:
+      return { ...state, fetchingRequest: false };
   }
 
   return state;
