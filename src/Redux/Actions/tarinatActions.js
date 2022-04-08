2a171f81907fda2e4f8a6832a66b284e25046ac1
MarekPuu <marek.puurunen@gmail.com>
2022-04-08 17:28:34 +0300

    Post tarina redux action

--- src/Redux/Actions/tarinatActions.js
+++ src/Redux/Actions/tarinatActions.js
@@ -1,6 +1,11 @@
 export const ADD_TARINA = 'ADD_TARINA';
 export const DELETE_TARINA = 'DELETE_TARINA';
 
+export const REQUEST_BEGIN = 'REQUEST_BEGIN';
+export const POST_TARINA_SUCCESS = 'POST_TARINA_SUCCESS';
+export const POST_TARINA_ERROR = 'POST_TARINA_ERROR';
+export const GET_TARINAT_SUCCESS = 'GET_TARINAT_SUCCESS';
+
 export function addTarina(payload) {
   return {
     type: ADD_TARINA,
@@ -11,5 +16,55 @@ export function deleteTarina(payload) {
   return {
     type: DELETE_TARINA,
     payload: payload,
+  };
+}
+
+export function postTarina(payload, axios, toast) {
+  const {
+    matkakohde,
+    yksityinen,
+    tarina,
+    tulopaiva,
+    lahtopaiva,
+    otsikko,
+    imgArr,
+  } = payload;
+  return async (dispatch) => {
+    try {
+      dispatch({ type: REQUEST_BEGIN });
+      try {
+        const formData = new FormData();
+        formData.append('matkakohde', matkakohde);
+        formData.append('yksityinen', yksityinen);
+        formData.append('alkupvm', tulopaiva);
+        formData.append('loppupvm', lahtopaiva);
+        formData.append('teksti', tarina);
+        formData.append('otsikko', otsikko);
+
+        imgArr.map((kuva) => {
+          formData.append('kuva', kuva);
+        });
+
+        const response = await axios.post('/api/tarina/tarina', formData);
+        if (response.status === 201) {
+          dispatch({
+            type: POST_TARINA_SUCCESS,
+            payload: response.data.savedTarina,
+          });
+          toast.success('Tarina lisätty', {
+            position: 'top-center',
+            duration: 1500,
+          });
+        }
+      } catch (error) {
+        dispatch({
+          type: POST_TARINA_ERROR,
+        });
+        toast.error(error.response.data.message, {
+          position: 'top-center',
+          duration: 1500,
+        });
+      }
+    } catch (error) {}
   };
 }
