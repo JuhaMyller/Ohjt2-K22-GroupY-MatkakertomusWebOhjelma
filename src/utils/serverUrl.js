e2e7c4e3915fd0960c2cc36c0a3960207a6a7953
MarekPuu <marek.puurunen@gmail.com>
2022-04-10 17:18:55 +0300

    ServerUrl

--- src/utils/serverUrl.js
+++ src/utils/serverUrl.js
@@ -1,2 +1,4 @@
-const SERVER_URL = "https://ohjelmistotuotanto2.herokuapp.com";
+const SERVER_URL = 'http://localhost:4000';
+//http://localhost:4000
+//https://ohjelmistotuotanto2.herokuapp.com
 export default SERVER_URL;
