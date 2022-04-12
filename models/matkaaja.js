c5c145d7e1fc27c2a64a70aaa79824881f324006
MarekPuu <marek.puurunen@gmail.com>
2022-04-12 20:55:03 +0300

    createdAt lisätty

--- models/matkaaja.js
+++ models/matkaaja.js
@@ -1,45 +1,48 @@
 const mongoose = require('mongoose');
 
-const MatkaajaSchema = mongoose.Schema({
-  etunimi: {
-    type: String,
-    required: true,
-    maxlength: 15,
+const MatkaajaSchema = mongoose.Schema(
+  {
+    etunimi: {
+      type: String,
+      required: true,
+      maxlength: 15,
+    },
+    sukunimi: {
+      type: String,
+      required: true,
+      maxlength: 20,
+    },
+    nimimerkki: {
+      type: String,
+      required: true,
+      maxlength: 15,
+      unique: true,
+    },
+    paikkakunta: {
+      type: String,
+    },
+    esittely: {
+      type: String,
+    },
+    kuva: {
+      type: String,
+    },
+    sposti: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salasana: {
+      type: String,
+      required: true,
+    },
+    refreshToken: { type: String },
+    palautaSposti: {
+      token: { type: String },
+      expiresAt: { type: Date },
+    },
   },
-  sukunimi: {
-    type: String,
-    required: true,
-    maxlength: 20,
-  },
-  nimimerkki: {
-    type: String,
-    required: true,
-    maxlength: 15,
-    unique: true,
-  },
-  paikkakunta: {
-    type: String,
-  },
-  esittely: {
-    type: String,
-  },
-  kuva: {
-    type: String,
-  },
-  sposti: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  salasana: {
-    type: String,
-    required: true,
-  },
-  refreshToken: { type: String },
-  palautaSposti: {
-    token: { type: String },
-    expiresAt: { type: Date },
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Matkaaja', MatkaajaSchema);
--- models/matkakohde.js
+++ models/matkakohde.js
@@ -1,31 +1,34 @@
 const mongoose = require('mongoose');
 
-const Matkakohde = mongoose.Schema({
-  kohdenimi: {
-    type: String,
-    required: true,
-  },
-  maa: {
-    type: String,
-    required: true,
-  },
-  paikkakunta: {
-    type: String,
-    required: true,
-  },
-  kuvateksti: {
-    type: String,
-    required: true,
-  },
-  kuva: {
-    type: String,
-  },
-  tarinat: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Tarina',
+const Matkakohde = mongoose.Schema(
+  {
+    kohdenimi: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    maa: {
+      type: String,
+      required: true,
+    },
+    paikkakunta: {
+      type: String,
+      required: true,
+    },
+    kuvateksti: {
+      type: String,
+      required: true,
+    },
+    kuva: {
+      type: String,
+    },
+    tarinat: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tarina',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Matkakohde', Matkakohde);
