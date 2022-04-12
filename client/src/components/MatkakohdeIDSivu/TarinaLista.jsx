f488ee050885a719c9abf2dc11dc2319cd0d3c23
Eero_Niskanen <eero.niskanen@edu.savonia.fi>
2022-04-12 16:26:01 +0300

    matkakohdesivun tarinasivu css animaatiota kuvalle

--- client/src/components/MatkakohdeIDSivu/TarinaLista.jsx
+++ client/src/components/MatkakohdeIDSivu/TarinaLista.jsx
@@ -54,9 +54,13 @@ const TarinaLista = ({ id }) => {
           </h2>
           <div className='divkuva'>
             <img
+              className='kuva'
               src={`${serverUrl}/img/${matkakohde.kuva}`}
               alt={`Matkakohteen ${matkakohde.kohdenimi} kuva`}
             />
+            <div className='kuvateksti'>
+              <h3>{matkakohde.kuvateksti}</h3>
+            </div>
           </div>
         </div>
       )}
