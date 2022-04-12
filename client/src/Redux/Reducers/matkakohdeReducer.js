08c54d6d238fb74222f79d3ed3e1c1d2ed727b8a
MarekPuu <marek.puurunen@gmail.com>
2022-04-12 14:17:30 +0300

    Matkakohteen muokkaus toimii

--- client/src/Redux/Reducers/matkakohdeReducer.js
+++ client/src/Redux/Reducers/matkakohdeReducer.js
@@ -6,6 +6,8 @@ import {
   POST_MATKAKOHDE_SUCCESS,
   POST_MATKAKOHDE_ERROR,
   GET_MATKAKOHTEET_SUCCESS,
+  PUT_MATKAKOHTEET_ERROR,
+  PUT_MATKAKOHTEET_SUCCESS,
 } from '../Actions/matkakohdeActions';
 const initialState = {
   Matkakohteet: [],
@@ -44,6 +46,19 @@ export default function matkakohdeReducer(state = initialState, action) {
         ),
       };
     case DELETE_MATKAKOHDE_ERROR:
+      return { ...state, fetchingRequest: false };
+    case PUT_MATKAKOHTEET_SUCCESS:
+      const paivitetty = state.Matkakohteet.map((m) => {
+        if (m._id === action.payload._id) return action.payload;
+        return m;
+      });
+
+      return {
+        ...state,
+        Matkakohteet: [...paivitetty],
+        fetchingRequest: false,
+      };
+    case PUT_MATKAKOHTEET_ERROR:
       return { ...state, fetchingRequest: false };
   }
 
