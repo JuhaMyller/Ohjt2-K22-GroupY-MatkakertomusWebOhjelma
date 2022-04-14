fc4b212a03bd8c21ebda9fec405c41986ac2c68c
MarekPuu <marek.puurunen@gmail.com>
2022-04-14 15:15:13 +0300

    Profiilin muokkaus submenu ja modal

---
+++ client/src/components/JasenetID/ProfiiliSubmenu.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useModalContext } from '../ResuableComponents/Modal/ModalContext';
+import MuokkaaProfiilia from '../ModalTemplates/MuokkaaProfiilia/MuokkaaProfiilia';
+import MuokkaaKuvaa from '../ModalTemplates/MuokkaaProfiilia/MuokkaaKuvaa';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { POISTA_KUVA_ONNISTUNEESTI } from '../../Redux/Actions/authActions';
+
+const ProfiiliSubmenu = ({ open, setOpen }) => {
+  const [fetching, setFetching] = useState(false);
+
+  const { openModal } = useModalContext();
+  const axios = useAxiosPrivate();
+  const dispatch = useDispatch();
+
+  const handleClick = (e, template) => {
+    setOpen(false);
+
+    openModal({ template: template, title: e.target.innerHTML });
+  };
+
+  const handlePoistaProfiilikuva = async () => {
+    if (fetching) return;
+    try {
+      setFetching(true);
+      const response = await axios.delete('/api/user/removeprofilepic');
+
+      if (response.status === 200) {
+        dispatch({ type: POISTA_KUVA_ONNISTUNEESTI });
+        setFetching(false);
+      }
+    } catch (error) {
+      setFetching(false);
+      toast.error(error.response.data.message, {
+        position: 'top-center',
+        duration: 1500,
+      });
+    }
+  };
+
+  return (
+    <Wrapper
+      onMouseLeave={() => setOpen(false)}
+      style={{ height: open ? '123px' : '0px' }}
+    >
+      <p onClick={(e) => handleClick(e, <MuokkaaProfiilia />)}>
+        Muokkaa profiilia
+      </p>
+      <p onClick={(e) => handleClick(e, <MuokkaaKuvaa />)}>Vaihda kuva</p>
+      <p onClick={handlePoistaProfiilikuva}>Poista profiilikuva</p>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: absolute;
+  transition: all 0.2s ease-out;
+  overflow: hidden;
+
+  left: 50%;
+  top: 30px;
+  z-index: 1000;
+  transform: translateX(-50%);
+  min-width: 200px;
+  background: grey;
+  p {
+    background-color: #c27a7a;
+    padding: 10px;
+    text-align: center;
+    color: white;
+    :not(:first-child) {
+      margin-top: 3px;
+    }
+  }
+`;
+
+export default ProfiiliSubmenu;
---
+++ client/src/components/ModalTemplates/MuokkaaProfiilia/MuokkaaProfiilia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import Input from '../../ResuableComponents/Input';
+import Button from '../../ResuableComponents/Button';
+
+const MuokkaaProfiilia = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled.div``;
+
+export default MuokkaaProfiilia;
