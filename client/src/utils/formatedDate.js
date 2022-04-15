f51b28732944694a20dd68e25267f396dfdc48a5
MarekPuu <marek.puurunen@gmail.com>
2022-04-15 19:26:21 +0300

    Formatoi päivän ja ajan omalla aikavyöhykkeellä

--- client/src/utils/formatedDate.js
+++ client/src/utils/formatedDate.js
@@ -7,4 +7,16 @@ const formatDate = (d) => {
   return `${day}.${month}.${year}`;
 };
 
+export const formatDateWithTime = (d) => {
+  const date = new Date(d);
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  const day = date.getDate();
+  const month = date.getUTCMonth() + 1;
+  const year = date.getUTCFullYear();
+
+  const time = date.toISOString().slice(11, 16);
+
+  return `${day}.${month}.${year} ${time}`;
+};
+
 export default formatDate;
