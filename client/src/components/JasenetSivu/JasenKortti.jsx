93b58d76c784bd79aaeea1ad23505101c98ffcf3
Remmeli <juha.myller@protonmail.com>
2022-04-21 11:22:24 +0300

    lisäämuutoksia

--- client/src/components/JasenetSivu/JasenKortti.jsx
+++ client/src/components/JasenetSivu/JasenKortti.jsx
@@ -1,4 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Input from '../ResuableComponents/Input';
+import Button from '../ResuableComponents/Button';
+import axios from '../../api/Axios';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import serverUrl from '../../utils/serverUrl';
+
 const JasenKortti = () => {
-  return <h1>Jasenkortti</h1>;
+  return (
+    <div className='kohdekortti' style={{ backgroundImage: `url(${kuva})` }}>
+      <div className='Jasenkortti'>
+        <h1>
+          {etunimi} {sukunimi}
+        </h1>
+        <h4>{nimimerkki}</h4>
+        <p>{esittely}</p>
+      </div>
+      <div className='kohdekortti_tarinat'>
+        <p>{createdAt}</p>
+        <p></p>
+      </div>
+    </div>
+  );
 };
 export default JasenKortti;
--- client/src/components/JasenetSivu/JasenetLista.jsx
+++ client/src/components/JasenetSivu/JasenetLista.jsx
@@ -1,10 +1,26 @@
 import JasenKortti from './JasenKortti';
+import React, { useEffect, useState } from 'react';
+import Input from '../ResuableComponents/Input';
+import Button from '../ResuableComponents/Button';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import serverUrl from '../../utils/serverUrl';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 
 const JasenetLista = () => {
+  const axios = useAxiosPrivate();
+
+  const haeMatkaaja = async () => {
+    const responseMatkaajat = await axios.get('/api/user');
+    console.log(responseMatkaajat);
+  };
+  useEffect(() => {
+    haeMatkaaja();
+  }, []);
   return (
-    <h1>
+    <div>
       <JasenKortti />
-    </h1>
+    </div>
   );
 };
 export default JasenetLista;
