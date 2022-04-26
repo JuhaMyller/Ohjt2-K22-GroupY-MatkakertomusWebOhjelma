03aa8ac59513604c94c44b4a33b7f845796e7051
MarekPuu <78322919+MarekPuu@users.noreply.github.com>
2022-04-26 14:36:07 +0300

    Error tilanteissa kuvan poistaminen serveriltä

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,27 @@
 //päätyy tähä catch blockista jossa errori on päästetty eteenpäin funtiolla =>  next(error)
 //vastaa clientille error statukset ja viestin
+const res = require('express/lib/response');
+const fs = require('fs');
+const util = require('util');
+const unlink = util.promisify(fs.unlink);
 
-const errorHandler = (error, req, res, next) => {
-  const { statusCode } = error;
-  console.error('Server Error: ', error);
-  res.status(statusCode || 500).json({ message: error.message });
+const errorHandler = async (error, req, res, next) => {
+  try {
+    const { statusCode } = error;
+    console.error('Server Error: ', error);
+    if (req.file) await unlink(req.file.path);
+    if (req.files) await handleUploadsFolder(req.files);
+    res.status(statusCode || 500).json({ message: error.message });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Odottamaton virhe' });
+  }
+};
+
+const handleUploadsFolder = async (files) => {
+  return files.map(async (file) => {
+    await unlink(file.path);
+  });
 };
 
 module.exports = errorHandler;
